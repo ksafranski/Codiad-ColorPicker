fcbb5833a2a7a7b1126aa476a2b22d0d726b8e35
jerrywham <github@ecyseo.net>
2013-06-09 02:40:05 +0300

    Copyright move under php tag to avoid session issue

--- dialog.php
+++ dialog.php
@@ -1,9 +1,9 @@
-<!--/*
+<?php
+/*
 *  Copyright (c) Codiad & Kent Safranski (codiad.com), distributed
 *  as-is and without warranty under the MIT License. See 
 *  [root]/license.txt for more. This information must remain intact.
-*/--> 
-<?php
+*/
 require_once('../../common.php');
 ?>
 <div id="colorpicker_region">
